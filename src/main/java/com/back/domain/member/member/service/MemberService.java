1798b1779d39fed8f79d30446b0616af92680520
jhs512 <jangka512@gmail.com>
2025-12-10 03:10:37 +0900

    c018

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.back.domain.member.member.service;
 
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.repository.MemberRepository;
+import com.back.global.exception.DomainException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class MemberService {
 
     public Member join(String username, String password, String nickname) {
         memberRepository.findByUsername(username).ifPresent(m -> {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+            throw new DomainException("409-1", "이미 존재하는 username 입니다.");
         });
 
         Member member = new Member(username, password, nickname);
