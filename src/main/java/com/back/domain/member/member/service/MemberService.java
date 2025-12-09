e2fbace464539038b57c190fb44f89f3bea8eb7e
jhs512 <jangka512@gmail.com>
2025-12-10 03:05:42 +0900

    c017

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -15,6 +15,10 @@ public class MemberService {
     }
 
     public Member join(String username, String password, String nickname) {
+        memberRepository.findByUsername(username).ifPresent(m -> {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        });
+
         Member member = new Member(username, password, nickname);
 
         return memberRepository.save(member);
