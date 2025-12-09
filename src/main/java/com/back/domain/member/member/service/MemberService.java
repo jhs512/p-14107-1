08543839d3657befd1e62449108aaeeddaaf4ba9
jhs512 <jangka512@gmail.com>
2025-12-10 01:38:29 +0900

    c012

---
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.back.domain.member.member.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    public long count() {
+        return 0;
+    }
+}
